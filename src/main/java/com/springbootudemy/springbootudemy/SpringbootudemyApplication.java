ebbc2087bedf7c16b85205d1f6a56c670ed9d56e
jt <jt@pop-os.localdomain>
2020-09-12 11:41:49 -0300

    Inicio do Projeto Spring

---
+++ src/main/java/com/springbootudemy/springbootudemy/SpringbootudemyApplication.java
@@ -0,0 +1,13 @@
+package com.springbootudemy.springbootudemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootudemyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootudemyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootudemy/springbootudemy/SpringbootudemyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootudemy.springbootudemy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootudemyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
